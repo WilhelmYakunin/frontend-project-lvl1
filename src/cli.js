09643191c14e1a2296ec0add77292ac4e4a90cfb
WilhelmYakunin <v.jakunin@inbox.ru>
2020-05-26 16:16:48 +0300

    step 3

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+; const sayHello = () => {
+    console.log('Welcome to the Brain Games!');
+
+    const userName = readlineSync.question('May I have your name? ');
+    console.log('Hello, ' + userName[0].toUpperCase() + userName.slice(1)  + '!');
+
+} 
+
+export default sayHello;
