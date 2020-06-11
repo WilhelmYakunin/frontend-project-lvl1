880df87a4d0314f2bde5d8d2af96f606b18ebd97
WilhelmYakunin <vit.yakunin@gmail.com>
2020-06-11 20:38:07 +0300

    refactoring of prime game function

--- src/games/isPrime.js
+++ src/games/isPrime.js
@@ -1,14 +1,14 @@
 import getRandomNum from '../getRandomNum.js';
 
+const isPrime = (num) => {
+  let coutIfNotPrime = 0;
+  for (let i = 2; i < Math.sqrt(num); i += 1) {
+    coutIfNotPrime += ((num % i === 0) ? 1 : 0);
+  } return (coutIfNotPrime === 0);
+};
+
 function prime() {
   const question = getRandomNum(4, 100);
-
-  const isPrime = (num) => {
-    let coutIfNotPrime = 0;
-    for (let i = 2; i < Math.sqrt(question); i += 1) {
-      coutIfNotPrime += ((num % i === 0) ? 1 : 0);
-    } return (coutIfNotPrime === 0);
-  };
 
   const rightAnswer = (isPrime(question)) ? 'yes' : 'no';
 
