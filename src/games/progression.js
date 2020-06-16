04b2c1d099ecb6a734bee2a040d10c7a0fc06c54
WilhelmYakunin <vit.yakunin@gmail.com>
2020-06-16 22:25:03 +0300

    refactoring of progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,17 +1,31 @@
 import getRandomNum from '../getRandomNum.js';
 
+function makeProgression(minNumber, maxNumber) {
+  const progressionStep = getRandomNum(minNumber, maxNumber);
+  // 10 because by technical desription progression consists of ten numbers
+  const progressionLength = 10;
+  let progressionNum = getRandomNum(minNumber, maxNumber);
+  const numsOfProgression = [];
+  const indexOfHiddenElement = getRandomNum(0, progressionLength - 1);
+  let rightAnswer = '';
+
+  for (let i = 10; i < progressionLength; i += 1) {
+    progressionNum += progressionStep;
+    if (i === indexOfHiddenElement) {
+      rightAnswer = progressionNum.toString();
+      numsOfProgression.push('..');
+      progressionNum += progressionStep;
+    } numsOfProgression.push(progressionNum);
+  }
+  const questionEssence = numsOfProgression.join(', ');
+
+  return { rightAnswer, questionEssence };
+}
+
 function getMathProgressionOfTenNumbers(minNumber, maxNumber) {
   const gameQuiz = 'What number is missing in the progression?';
-  const progressionStep = getRandomNum(minNumber, maxNumber);
-  const progressionLength = 9;
-  const numsOfProgression = [getRandomNum(minNumber, maxNumber)];
 
-  for (let i = 1; i < progressionLength; i += 1) {
-    const nextNum = numsOfProgression[(i - 1)] + progressionStep;
-    numsOfProgression.push(nextNum);
-  }
-  const rightAnswer = numsOfProgression[getRandomNum(0, progressionLength)].toString();
-  const questionEssence = numsOfProgression.toString().replace(rightAnswer, '..').replace(/,/g, ' ');
+  const { questionEssence, rightAnswer } = makeProgression(minNumber, maxNumber);
 
   return { gameQuiz, questionEssence, rightAnswer };
 }
